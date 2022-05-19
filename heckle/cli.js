275e52fb950b2f5b45092690d04bec94a3eadadb
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-19 08:32:01 -0700

    Add target ID via command

--- heckle/cli.js
+++ heckle/cli.js
@@ -8,100 +8,99 @@ const { cosmiconfig } = require("cosmiconfig");
 
 const { run } = require("./heckle");
 
-program
-  .argument("[operation]")
-  .argument("[target]")
-  .action(async (operation, cliTarget, options) => {
-    const explorer = cosmiconfig("heckle");
-    const { config } = await explorer.search();
+program.argument("name").action(async (name, options) => {
+  const explorer = cosmiconfig("heckle");
+  const { config } = await explorer.search();
 
-    const target = cliTarget || config.target;
+  const target = config?.targets?.[name];
+  const operation = null;
 
-    if (!target) {
-      console.error("No target found!");
-      process.exit(1);
-    }
-
-    const displayTarget = target.match(/^([^\?]*)/)[1];
-
+  if (!target) {
+    console.error(`Target "${name}" not found!`);
     console.log();
-    console.log(`${chalk.bold(`Health Check:`)} ${chalk.gray(displayTarget)}`);
+    process.exit(1);
+  }
+
+  const displayTarget = target.match(/^([^\?]*)/)[1];
+
+  console.log();
+  console.log(`${chalk.bold(`Health Check:`)} ${chalk.gray(displayTarget)}`);
+  console.log();
+
+  const result = await run(operation, target);
+
+  if (!result) {
+    console.error(chalk.red("Service not found"));
     console.log();
+    process.exit(1);
+  }
 
-    const result = await run(operation, target);
+  const hex = {
+    good: "#0dd332",
+    bad: "#d30d1c",
+  };
 
-    if (!result) {
-      console.error(chalk.red("Service not found"));
-      console.log();
-      process.exit(1);
-    }
-
-    const hex = {
-      good: "#0dd332",
-      bad: "#d30d1c",
-    };
-
-    const summary = Object.keys(result.checks).reduce(
-      (acc, check) => {
-        acc.count++;
-        const checkResult = result.checks[check];
-        if (checkResult.healthy) {
-          acc.healthy.count++;
-          acc.healthy.checks[check] = checkResult;
-        } else {
-          acc.unhealthy.count++;
-          acc.unhealthy.checks[check] = checkResult;
-        }
-        return acc;
-      },
-      {
-        count: 0,
-        healthy: { count: 0, checks: {} },
-        unhealthy: { count: 0, checks: {} },
+  const summary = Object.keys(result.checks).reduce(
+    (acc, check) => {
+      acc.count++;
+      const checkResult = result.checks[check];
+      if (checkResult.healthy) {
+        acc.healthy.count++;
+        acc.healthy.checks[check] = checkResult;
+      } else {
+        acc.unhealthy.count++;
+        acc.unhealthy.checks[check] = checkResult;
       }
+      return acc;
+    },
+    {
+      count: 0,
+      healthy: { count: 0, checks: {} },
+      unhealthy: { count: 0, checks: {} },
+    }
+  );
+
+  // console.log(summary);
+  if (summary.unhealthy.count === 0) {
+    console.log(chalk.green.bold(`All ${summary.count} checks passed`));
+  } else if (summary.unhealthy.count === summary.count) {
+    console.log(chalk.red.bold(`All ${summary.count} checks failed`));
+  } else {
+    console.log(
+      chalk.red.bold(
+        `${summary.unhealthy.count} out of ${summary.count} checks failed`
+      )
     );
+  }
+  console.log();
 
-    // console.log(summary);
-    if (summary.unhealthy.count === 0) {
-      console.log(chalk.green.bold(`All ${summary.count} checks passed`));
-    } else if (summary.unhealthy.count === summary.count) {
-      console.log(chalk.red.bold(`All ${summary.count} checks failed`));
-    } else {
+  if (summary.healthy.count > 0) {
+    console.log(chalk.white.bold.bgGreen(" HEALTHY "));
+
+    Object.keys(summary.healthy.checks).forEach((check) => {
+      const checkResult = summary.healthy.checks[check];
       console.log(
-        chalk.red.bold(
-          `${summary.unhealthy.count} out of ${summary.count} checks failed`
-        )
+        `${chalk.green.bold("✔")} ${checkResult.description} ${chalk.gray(
+          `(${check})`
+        )}`
       );
-    }
+    });
     console.log();
+  }
+  if (summary.unhealthy.count > 0) {
+    console.log(chalk.white.bold.bgRed(" UNHEALTHY "));
 
-    if (summary.healthy.count > 0) {
-      console.log(chalk.white.bold.bgGreen(" HEALTHY "));
+    Object.keys(summary.unhealthy.checks).forEach((check) => {
+      const checkResult = summary.unhealthy.checks[check];
 
-      Object.keys(summary.healthy.checks).forEach((check) => {
-        const checkResult = summary.healthy.checks[check];
-        console.log(
-          `${chalk.green.bold("✔")} ${checkResult.description} ${chalk.gray(
-            `(${check})`
-          )}`
-        );
-      });
-      console.log();
-    }
-    if (summary.unhealthy.count > 0) {
-      console.log(chalk.white.bold.bgRed(" UNHEALTHY "));
-
-      Object.keys(summary.unhealthy.checks).forEach((check) => {
-        const checkResult = summary.unhealthy.checks[check];
-
-        console.log(
-          `${chalk.red.bold("×")} ${checkResult.error} ${chalk.gray(
-            `(${check})`
-          )}`
-        );
-      });
-      console.log();
-    }
-  });
+      console.log(
+        `${chalk.red.bold("×")} ${checkResult.error} ${chalk.gray(
+          `(${check})`
+        )}`
+      );
+    });
+    console.log();
+  }
+});
 
 program.parse();
