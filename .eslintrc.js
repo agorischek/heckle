58864a94743ae2ee9c57805a23f49b9cdd4bfb6d
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-19 19:05:54 -0700

    Add lint and format configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+  ignorePatterns: ['dist/*'],
 };
