5c2dd7276659bfaa6e3879e070a997835c31aa35
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-21 14:19:38 -0700

    Factor out printers

---
+++ packages/heckle-cli/src/printers/printIntro.ts
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+import { Target } from 'heckle-core';
+
+export function printIntro(target: Target) {
+  console.log();
+  console.log(
+    `${chalk.bold(`Health Check:`)} ${chalk.gray(target.displayEndpoint)}`
+  );
+  console.log();
+}
---
+++ packages/heckle-cli/src/printers/printReport.ts
@@ -0,0 +1,46 @@
+import chalk from 'chalk';
+
+import { HealthReport } from '../types/HealthReport';
+
+export function printReport(report: HealthReport) {
+  if (report.unhealthy.count === 0) {
+    console.log(chalk.green.bold(`All ${report.count} checks passed`));
+  } else if (report.unhealthy.count === report.count) {
+    console.log(chalk.red.bold(`All ${report.count} checks failed`));
+  } else {
+    console.log(
+      chalk.red.bold(
+        `${report.unhealthy.count} out of ${report.count} checks failed`
+      )
+    );
+  }
+  console.log();
+
+  if (report.healthy.count > 0) {
+    console.log(chalk.white.bold.bgGreen(' HEALTHY '));
+
+    Object.keys(report.healthy.checks).forEach((check) => {
+      const checkResult = report.healthy.checks[check];
+      console.log(
+        `${chalk.green.bold('✔')} ${checkResult.description} ${chalk.gray(
+          `(${check})`
+        )}`
+      );
+    });
+    console.log();
+  }
+  if (report.unhealthy.count > 0) {
+    console.log(chalk.white.bold.bgRed(' UNHEALTHY '));
+
+    Object.keys(report.unhealthy.checks).forEach((check) => {
+      const checkResult = report.unhealthy.checks[check];
+
+      console.log(
+        `${chalk.red.bold('×')} ${checkResult.error} ${chalk.gray(
+          `(${check})`
+        )}`
+      );
+    });
+    console.log();
+  }
+}
