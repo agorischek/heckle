c8e1f93b902775912a3893b17af22176bd6b2a5f
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-21 16:19:36 -0700

    Add call and ensure

---
+++ packages/heckle-core/src/call.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+import { getConfig } from './getConfig';
+
+export async function call(route: string, method = 'get', payload: unknown) {
+  const config = await getConfig();
+  const base = config.root ? `${config.root}${route}` : route;
+  const url = new URL(base);
+  if (config.params) {
+    Object.keys(config.params).forEach((param) => {
+      if (config.params?.[param] && !url.searchParams.has(param))
+        url.searchParams.set(param, config.params[param]);
+    });
+  }
+  const target = url.toString();
+
+  const response = await axios.request({
+    method,
+    url: target,
+    data: payload,
+    validateStatus: function (status: number) {
+      return Boolean(status);
+    },
+  });
+  console.log(response);
+  return response;
+}
