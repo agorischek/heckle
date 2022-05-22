c0e4760e0028d606d197c33ef5007d841c7961a2
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-22 09:09:46 -0700

    Rename packages

--- packages/cli/src/types/HealthReport.ts
+++ packages/cli/src/types/HealthReport.ts
@@ -1,4 +1,4 @@
-import { HealthCheckResult, HealthSummary } from '@heckle/core/src/types';
+import { HealthCheckResult, HealthSummary } from '@heckle/health/src/types';
 
 export class HealthReport {
   count: number;
