8cf1e481a57a868d3572eeb5facc2ae00d3bd77a
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-22 08:58:29 -0700

    Add `ensure` messages

--- packages/heckle-core/src/ensure.ts
+++ packages/heckle-core/src/ensure.ts
@@ -2,25 +2,35 @@ import { AxiosResponse } from 'axios';
 
 export function ensure(reply: AxiosResponse) {
   return {
-    informational: function () {
+    informational: function (message: string) {
       if (!String(reply.status).match(/^1../))
-        throw `Expected status ${reply.status} to be Informational (1xx)`;
+        throw (
+          message || `Expected status ${reply.status} to be Informational (1xx)`
+        );
     },
-    successful: function () {
+    successful: function (message: string) {
       if (!String(reply.status).match(/^2../))
-        throw `Expected status ${reply.status} to be Successful (2xx)`;
+        throw (
+          message || `Expected status ${reply.status} to be Successful (2xx)`
+        );
     },
-    redirection: function () {
+    redirection: function (message: string) {
       if (!String(reply.status).match(/^3../))
-        throw `Expected status ${reply.status} to be Redirection (3xx)`;
+        throw (
+          message || `Expected status ${reply.status} to be Redirection (3xx)`
+        );
     },
-    clientError: function () {
+    clientError: function (message: string) {
       if (!String(reply.status).match(/^4../))
-        throw `Expected status ${reply.status} to be Client Error (4xx)`;
+        throw (
+          message || `Expected status ${reply.status} to be Client Error (4xx)`
+        );
     },
-    serverError: function () {
+    serverError: function (message: string) {
       if (!String(reply.status).match(/^5../))
-        throw `Expected status ${reply.status} to be Server Error (5xx)`;
+        throw (
+          message || `Expected status ${reply.status} to be Server Error (5xx)`
+        );
     },
   };
 }
