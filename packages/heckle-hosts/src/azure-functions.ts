c7d5508a723723da0d9225b6431a2930236ede18
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-21 18:44:10 -0700

    Fix hosts export

--- packages/heckle-hosts/src/azure-functions.ts
+++ packages/heckle-hosts/src/azure-functions.ts
@@ -1,10 +1,10 @@
-import { getConfig, run } from '@heckle/core';
+import { getConfig, run as heckle } from '@heckle/core';
 import { Context } from '@azure/functions';
 
-export async function health(context: Context) {
+export async function run(context: Context) {
   const config = await getConfig();
 
-  const result = await run(config);
+  const result = await heckle(config);
 
   const status = result.healthy
     ? config?.codes?.healthy || 200
