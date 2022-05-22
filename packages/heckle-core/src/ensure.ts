7eb0f10584ce7e1e194b899169fdf19af03db5b9
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-21 18:44:02 -0700

    Fix ensure assertion

--- packages/heckle-core/src/ensure.ts
+++ packages/heckle-core/src/ensure.ts
@@ -3,23 +3,23 @@ import { AxiosResponse } from 'axios';
 export function ensure(reply: AxiosResponse) {
   return {
     informational: function () {
-      if (String(reply.status).match(/^1../))
+      if (!String(reply.status).match(/^1../))
         throw `Expected status ${reply.status} not to be Informational (1xx)`;
     },
     successful: function () {
-      if (String(reply.status).match(/^2../))
+      if (!String(reply.status).match(/^2../))
         throw `Expected status ${reply.status} not to be Successful (2xx)`;
     },
     redirection: function () {
-      if (String(reply.status).match(/^3../))
+      if (!String(reply.status).match(/^3../))
         throw `Expected status ${reply.status} not to be Redirection (3xx)`;
     },
     clientError: function () {
-      if (String(reply.status).match(/^4../))
+      if (!String(reply.status).match(/^4../))
         throw `Expected status ${reply.status} not to be Client Error (4xx)`;
     },
     serverError: function () {
-      if (String(reply.status).match(/^5../))
+      if (!String(reply.status).match(/^5../))
         throw `Expected status ${reply.status} not to be Server Error (5xx)`;
     },
   };
