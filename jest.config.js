463f7df4b6d86999d95702a7702feee63cdae8cd
Alex Gorischek <alex.gorischek@gmail.com>
2022-05-26 18:31:51 -0700

    Collect coverage from all files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['dist'],
-  coveragePathIgnorePatterns: ['node_modules'],
+  coveragePathIgnorePatterns: ['node_modules', 'dist'],
+  collectCoverageFrom: ['packages/**/*.ts'],
   coverageThreshold: {
     global: {
       branches: 90,
